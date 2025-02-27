16a341f7c195ac15b922d17716ea8838c718d33c
Mingyang Han <Mingyang_Han@esri.com>
2025-02-27 17:49:32 +0800

    chore: revert for test

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react'
 import * as ReactDOMClient from 'react-dom/client'
-import Component from './component'
 
 const styles = css`
   background-color: #d81616;
@@ -17,7 +16,7 @@ const Root = (props) => {
         style={{ height: 600, width: 800 }}
         className='border'
       >
-        <Component />
+        Test
       </div>
     </div>
   )
